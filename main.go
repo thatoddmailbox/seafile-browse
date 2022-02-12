2c8a6482028010ba8eeb2b977bcd929d6d595a07
Alex Studer <alex@studer.dev>
2022-02-11 23:57:31 -0500

    remove flag code (it's a config file now)

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"flag"
 	"log"
 )
 
 func main() {
 	log.Println("seafile-browse")
 
-	directory := flag.String("d", "", "The seafile-data directory.")
 
-	flag.Parse()
 
-	if *directory == "" {
-		log.Fatalln("The directory (d) flag is required.")
-	}
 }
