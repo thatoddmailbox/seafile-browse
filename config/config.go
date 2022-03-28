ea9ae5af4231ca69866b60a3fe1cf6eaa0b1e8a4
Alex Studer <alex@studer.dev>
2022-03-27 21:37:47 -0400

    config: load file, determine a location type, and set up a fs.FS to use

---
+++ config/config.go
@@ -0,0 +1,90 @@
+package config
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/thatoddmailbox/sftpfs"
+	"golang.org/x/crypto/ssh"
+)
+
+type Config struct {
+	Location struct {
+		Local *struct {
+			Path string
+		}
+		SFTP *struct {
+			Host     string
+			User     string
+			Password string
+			Path     string
+		}
+	}
+
+	f      fs.FS
+	sftpFS *sftpfs.Client
+}
+
+func (c *Config) initFS() error {
+	var err error
+
+	if c.Location.Local != nil {
+		c.f = os.DirFS(c.Location.Local.Path)
+		return nil
+	}
+
+	if c.Location.SFTP != nil {
+		c.sftpFS, err = sftpfs.Dial("tcp", c.Location.SFTP.Host, &ssh.ClientConfig{
+			User: c.Location.SFTP.User,
+			Auth: []ssh.AuthMethod{
+				ssh.Password(c.Location.SFTP.Password),
+			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		})
+		if err != nil {
+			return err
+		}
+
+		c.f = c.sftpFS
+
+		if c.Location.SFTP.Path != "" {
+			subFS, err := fs.Sub(c.sftpFS, c.Location.SFTP.Path)
+			if err != nil {
+				return err
+			}
+
+			c.f = subFS
+		}
+
+		return nil
+	}
+
+	return errors.New("config: could not determine location type")
+}
+
+func (c *Config) Close() {
+	if c.Location.SFTP != nil {
+		c.sftpFS.Close()
+	}
+}
+
+func (c *Config) FS() fs.FS {
+	return c.f
+}
+
+func Load(path string) (*Config, error) {
+	c := Config{}
+	_, err := toml.DecodeFile(path, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.initFS()
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
