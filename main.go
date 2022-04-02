41651fad5b12ebded8e5b87ee3f918142f43cd12
Alex Studer <alex@studer.dev>
2022-04-02 02:14:22 -0400

    create repo FS map at start, and allow seeing all repos under same server

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/thatoddmailbox/fsbrowse"
 	"github.com/thatoddmailbox/seafile-browse/config"
@@ -20,32 +22,61 @@ func main() {
 	defer cfg.Close()
 
 	storage := seafile.NewStorageWithFS(cfg.FS())
-	repos, err := storage.ListRepoIDs()
+	repoIDs, err := storage.ListRepoIDs()
 	if err != nil {
 		panic(err)
 	}
 
-	log.Println(repos)
+	repos := map[string]*seafile.Repo{}
+	repoLatestFSHandler := map[string]http.Handler{}
+	for _, repoID := range repoIDs {
+		repos[repoID], err = storage.OpenRepo(repoID)
+		if err != nil {
+			panic(err)
+		}
 
-	repo, err := storage.OpenRepo(repos[0])
-	if err != nil {
-		panic(err)
-	}
-	log.Println(repo)
+		commit, err := repos[repoID].GetLatestCommit()
+		if err != nil {
+			panic(err)
+		}
 
-	commit, err := repo.GetLatestCommit()
-	if err != nil {
-		panic(err)
-	}
-	log.Println(commit)
+		latestFS, err := commit.GetFS()
+		if err != nil {
+			panic(err)
+		}
 
-	fsys, err := commit.GetFS()
-	if err != nil {
-		panic(err)
+		repoLatestFSHandler[repoID] = fsbrowse.FileServer(latestFS)
 	}
-	log.Println(fsys)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			// repo list
+			fmt.Fprintf(w, "Repo list TODO")
+			return
+		}
+
+		// repo contents
+		path := r.URL.Path[1:]
+		if strings.HasSuffix(path, "/") {
+			path = path[:len(path)-1]
+		}
+
+		pathParts := strings.Split(path, "/")
+		repoID := pathParts[0]
+		repoPath := pathParts[1:]
+
+		repoHandler, repoExists := repoLatestFSHandler[repoID]
+		if !repoExists {
+			fmt.Fprintf(w, "Repo ID invalid")
+			return
+		}
+
+		// kinda janky, we reuse the request but rewrite its path
+		r.URL.Path = strings.Join(repoPath, "/")
+		repoHandler.ServeHTTP(w, r)
+	})
 
 	port := 9253
 	log.Printf("Listening on port %d...", port)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), fsbrowse.FileServer(fsys)))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
