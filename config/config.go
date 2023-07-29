920603d279c0dafb8a1fa0b457a3178bd751dbd3
Alex Studer <alex@studer.dev>
2023-07-29 18:27:48 -0400

    add snapshot support

--- config/config.go
+++ config/config.go
@@ -13,17 +13,22 @@ import (
 type Config struct {
 	Location struct {
 		Local *struct {
-			Path string
+			Path         string
+			SnapshotPath string
 		}
 		SFTP *struct {
-			Host     string
-			User     string
-			Password string
-			Path     string
+			Host         string
+			User         string
+			Password     string
+			Path         string
+			SnapshotPath string
 		}
 	}
 
+	path string
+
 	f      fs.FS
+	sf     fs.FS
 	sftpFS *sftpfs.Client
 }
 
@@ -32,6 +37,10 @@ func (c *Config) initFS() error {
 
 	if c.Location.Local != nil {
 		c.f = os.DirFS(c.Location.Local.Path)
+		if c.Location.Local.SnapshotPath != "" {
+			c.sf = os.DirFS(c.Location.Local.SnapshotPath)
+		}
+		c.path = c.Location.Local.Path
 		return nil
 	}
 
@@ -48,6 +57,7 @@ func (c *Config) initFS() error {
 		}
 
 		c.f = c.sftpFS
+		c.path = c.Location.SFTP.Path
 
 		if c.Location.SFTP.Path != "" {
 			subFS, err := fs.Sub(c.sftpFS, c.Location.SFTP.Path)
@@ -56,6 +66,15 @@ func (c *Config) initFS() error {
 			}
 
 			c.f = subFS
+		}
+
+		if c.Location.SFTP.SnapshotPath != "" {
+			subFS, err := fs.Sub(c.sftpFS, c.Location.SFTP.SnapshotPath)
+			if err != nil {
+				return err
+			}
+
+			c.sf = subFS
 		}
 
 		return nil
@@ -70,8 +89,20 @@ func (c *Config) Close() {
 	}
 }
 
+func (c *Config) Path() string {
+	return c.path
+}
+
 func (c *Config) FS() fs.FS {
 	return c.f
+}
+
+func (c *Config) SnapshotFS() fs.FS {
+	return c.sf
+}
+
+func (c *Config) HaveSnapshots() bool {
+	return c.sf != nil
 }
 
 func Load(path string) (*Config, error) {
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"path"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -14,16 +16,34 @@ import (
 	"github.com/thatoddmailbox/seafile-browse/seafile"
 )
 
-func main() {
-	log.Println("seafile-browse")
+func renderRepoList(w http.ResponseWriter, r *http.Request, snapshotName string) {
 
-	cfg, err := config.Load("config.toml")
-	if err != nil {
-		panic(err)
+}
+
+type repoInfo struct {
+	repoIDs []string
+	repos   map[string]*seafile.Repo
+	repoFSs map[string]fs.FS
+}
+
+var allRepoInfo map[string]repoInfo = map[string]repoInfo{}
+
+func getRepoInfoForSnapshot(snapshot string, cfg *config.Config) ([]string, map[string]*seafile.Repo, map[string]fs.FS) {
+	ri, exists := allRepoInfo[snapshot]
+	if exists {
+		return ri.repoIDs, ri.repos, ri.repoFSs
 	}
-	defer cfg.Close()
 
-	storage := seafile.NewStorageWithFS(cfg.FS())
+	f := cfg.FS()
+	if snapshot != "" {
+		var err error
+		f, err = fs.Sub(cfg.SnapshotFS(), path.Join(snapshot, cfg.Path()))
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	storage := seafile.NewStorageWithFS(f)
 	repoIDs, err := storage.ListRepoIDs()
 	if err != nil {
 		panic(err)
@@ -47,14 +67,75 @@ func main() {
 			panic(err)
 		}
 
-		repoLatestFSHandler[repoID] = fsbrowse.FileServer(latestFS)
+		repoFSs[repoID] = latestFS
+	}
+
+	allRepoInfo[snapshot] = repoInfo{repoIDs, repos, repoFSs}
+
+	return repoIDs, repos, repoFSs
+}
+
+func main() {
+	log.Println("seafile-browse")
+
+	cfg, err := config.Load("config.toml")
+	if err != nil {
+		panic(err)
+	}
+	defer cfg.Close()
+
+	snapshots := []string{}
+	if cfg.HaveSnapshots() {
+		sf := cfg.SnapshotFS()
+		entries, err := fs.ReadDir(sf, ".")
+		if err != nil {
+			panic(err)
+		}
+
+		for _, entry := range entries {
+			if entry.IsDir() {
+				snapshots = append(snapshots, entry.Name())
+			}
+		}
+
+		sort.Strings(snapshots)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/" {
+		path := r.URL.Path[1:]
+		path = strings.TrimSuffix(path, "/")
+
+		activeSnapshot := ""
+
+		pathParts := strings.Split(path, "/")
+		if len(pathParts) > 0 {
+			if strings.HasPrefix(pathParts[0], "snapshot:") {
+				activeSnapshot = strings.SplitN(pathParts[0], ":", 2)[1]
+				pathParts = pathParts[1:]
+			}
+		}
+
+		notice := ""
+		if cfg.HaveSnapshots() {
+			if activeSnapshot == "" {
+				notice = "You are viewing the latest data."
+			} else {
+				notice = "You are viewing snapshot <code>" + html.EscapeString(activeSnapshot) + "</code>."
+			}
+
+			notice += " <a href=\"snapshots/\">View snapshots</a><br><br>"
+		}
+
+		_, repos, repoFSs := getRepoInfoForSnapshot(activeSnapshot, cfg)
+
+		if len(pathParts) == 0 || path == "" {
 			// repo list
 			w.Header().Set("Content-Type", "text/html")
-			fmt.Fprintf(w, "<html><head><title>seafile-browse</title><body>Select a library:<ul>")
+			fmt.Fprintf(w, "<html><head><title>seafile-browse</title><body>")
+			if cfg.HaveSnapshots() {
+				fmt.Fprint(w, notice)
+			}
+			fmt.Fprintf(w, "Select a library:<ul>")
 			for repoID, _ := range repos {
 				fmt.Fprintf(w, "<li><a href=\"%s/\">%s</li>", html.EscapeString(repoID), html.EscapeString(repoID))
 			}
@@ -63,22 +144,31 @@ func main() {
 		}
 
 		// repo contents
-		path := r.URL.Path[1:]
-		path = strings.TrimSuffix(path, "/")
 
-		pathParts := strings.Split(path, "/")
 		repoID := pathParts[0]
 		repoPath := pathParts[1:]
+
+		if repoID == "snapshots" {
+			// snapshots list
+			w.Header().Set("Content-Type", "text/html")
+			fmt.Fprintf(w, "<html><head><title>seafile-browse</title><body>")
+			fmt.Fprintf(w, "<a href=\"/\">View latest data</a><br><br>")
+			fmt.Fprintf(w, "Or, select a snapshot:<ul>")
+			for _, snapshot := range snapshots {
+				fmt.Fprintf(w, "<li><a href=\"/snapshot:%s/\">%s</li>", html.EscapeString(snapshot), html.EscapeString(snapshot))
+			}
+			fmt.Fprintf(w, "</ul></body></html>")
+			return
+		}
 
 		repoFS, repoExists := repoFSs[repoID]
 		if !repoExists {
 			fmt.Fprintf(w, "Repo ID invalid")
 			return
 		}
-
 		// kinda janky, we reuse the request but rewrite its path
 		r.URL.Path = strings.Join(repoPath, "/")
-		fsbrowse.ServeHTTPStateless(w, r, repoFS, "", "")
+		fsbrowse.ServeHTTPStateless(w, r, repoFS, "", notice)
 	})
 
 	port := 9253
